50374e88435e0ff2410cbaeafde1613699931c85
MattFrz <your@email.com>
2026-04-04 16:59:33 -0400

    feat: add missing skills from resume

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,14 +17,13 @@ const Skills = () => {
         <div className="skill-tags">
           <span>Python</span>
           <span>Java</span>
-          <span>C++</span>
+          <span>C/C++</span>
           <span>JavaScript</span>
           <span>TypeScript</span>
           <span>SQL</span>
+          <span>R</span>
           <span>HTML</span>
           <span>CSS</span>
-          <span>R</span>
-          <span>ARM Assembly</span>
         </div>
       </div>
 
@@ -34,11 +33,22 @@ const Skills = () => {
           <span>React</span>
           <span>Node.js</span>
           <span>Express.js</span>
+          <span>Spring Boot</span>
+          <span>Flask</span>
+          <span>FastAPI</span>
           <span>TensorFlow</span>
+          <span>PyTorch</span>
           <span>Pandas</span>
           <span>NumPy</span>
+          <span>Scikit-learn</span>
+          <span>Matplotlib</span>
+          <span>Bootstrap</span>
           <span>Qt 6</span>
           <span>Stellar SDK</span>
+          <span>JUnit</span>
+          <span>Jest</span>
+          <span>Jasmine</span>
+          <span>Karma</span>
         </div>
       </div>
 
@@ -47,13 +57,24 @@ const Skills = () => {
         <div className="skill-tags">
           <span>Git</span>
           <span>GitHub</span>
+          <span>Linux</span>
+          <span>UNIX</span>
+          <span>Windows</span>
+          <span>REST APIs</span>
+          <span>Postman</span>
+          <span>Jira</span>
+          <span>Docker</span>
           <span>AWS</span>
+          <span>CI/CD</span>
+          <span>MongoDB</span>
           <span>MySQL</span>
+          <span>PostgreSQL</span>
           <span>SQLite</span>
+          <span>Tableau</span>
+          <span>Alteryx</span>
           <span>Stripe</span>
           <span>CMake</span>
-          <span>Linux</span>
-          <span>Jira</span>
+          <span>Microsoft Office</span>
           <span>VS Code</span>
         </div>
       </div>
